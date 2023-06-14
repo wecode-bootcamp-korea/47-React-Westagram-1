424d5f69951f192fbd6cef440adc47321889474a
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-14 15:44:35 +0900

    Add: 초기 세팅 완료

---
+++ README.md
@@ -0,0 +1,1 @@
+Front Westagram | React 팀 프로젝트 - 최진이, 이지원
---
+++ src/Router.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// 최진이의 컴포넌트
+import LoginJini from './pages/jiniChoi/Login/Login';
+import MainJini from './pages/jiniChoi/Main/Main';
+
+// 이지원의 컴포넌트
+import LoginJiwon from './pages/jiwonLee/Login/Login';
+import MainJiwon from './pages/jiwonLee/Main/Main';
+
+// import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-jini" element={<LoginJini />} />
+        <Route path="/main-jini" element={<MainJini />} />
+        <Route path="/login-jiwon" element={<LoginJiwon />} />
+        <Route path="/main-jiwon" element={<MainJiwon />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
