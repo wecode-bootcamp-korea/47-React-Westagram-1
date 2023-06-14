8f63439d99742e5750f70a3da814534586facdce
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-14 16:29:17 +0900

    Modify : 피드백 수정 완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/reset.scss';
+import './styles/common.scss';
+import './styles/variables.scss';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
