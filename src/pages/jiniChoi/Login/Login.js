2d0dd45519699175176f719827a9772319c19f94
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-16 11:05:03 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jiniChoi/Login/Login.js
+++ src/pages/jiniChoi/Login/Login.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
 
+  const [id, setID] = useState('');
+  const [pw, setPw] = useState('');
+
+  const saveUserId = event => {
+    setID(event.target.value);
+  };
+
+  const saveUserPw = event => {
+    setPw(event.target.value);
+  };
+
   return (
-    <div class="loginBox">
-      <div class="logo">
+    <div className="loginBox">
+      <div className="logo">
         <h1>Westagram</h1>
       </div>
-      <div class="loginInput">
+      <div className="loginInput">
         <form>
           <input
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             id="id"
+            value={id}
+            onChange={saveUserId}
           />
-          <input type="password" placeholder="비밀번호" id="pw" />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            id="pw"
+            value={pw}
+            onChange={saveUserPw}
+          />
           {/* <Link to="/main">이동</Link> */}
           <button onClick={() => navigate('/main')} className="loginBtn">
             로그인
@@ -27,7 +46,7 @@ const Login = () => {
           </button> */}
         </form>
       </div>
-      <span class="findPassword">비밀번호를 잊으셨나요?</span>
+      <span className="findPassword">비밀번호를 잊으셨나요?</span>
     </div>
   );
 };
