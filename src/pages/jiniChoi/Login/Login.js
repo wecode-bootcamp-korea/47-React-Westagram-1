749b4867e2baa49c88748770af39bf87782790f5
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-22 16:34:39 +0900

    backend 연결 : Login & Signup

--- src/pages/jiniChoi/Login/Login.js
+++ src/pages/jiniChoi/Login/Login.js
@@ -3,6 +3,65 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  const signUp = () => {
+    fetch('http://10.58.52.76:3000/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        // email: 'jjinnnbb@gmail.com',
+        // password: 'abcd1237674@#',
+        email: inputValues.id,
+        password: inputValues.pw,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        alert('회원가입 실패');
+      })
+      .then(data => {
+        if (data.message === 'signUp success') {
+          localStorage.setItem('TOKEN', data.token);
+          alert('회원가입 성공');
+        } else {
+          alert('회원가입 실패');
+        }
+      });
+  };
+
+  const login = () => {
+    fetch('http://10.58.52.76:3000/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        // email: 'jjinnn@gmail.com',
+        // password: 'abcd1234@#',
+        email: inputValues.id,
+        password: inputValues.pw,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        alert('로그인 실패');
+      })
+      .then(data => {
+        if (data.message === 'Login success') {
+          localStorage.setItem('TOKEN', data.token);
+          alert('로그인 성공');
+          navigate('/main-jini');
+        } else if (data.message === 'Invalid email or password') {
+          alert('로그인 실패');
+        }
+      });
+  };
+
   const navigate = useNavigate();
 
   const [inputValues, setInputValues] = useState({
@@ -28,7 +87,7 @@ const Login = () => {
         <h1>Westagram</h1>
       </div>
       <div className="loginInput">
-        <form>
+        <div className="inputBox">
           <input
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
@@ -47,9 +106,8 @@ const Login = () => {
             onKeyUp={passedLogin}
             name="pw"
           />
-          {/* <Link to="/main">이동</Link> */}
           <button
-            onClick={() => navigate('/main-jini')}
+            onClick={login}
             className={loginBtn ? 'activeLoginBtn' : 'loginBtn'}
             disabled={
               inputValues.id === '' || inputValues.pw === '' ? true : false
@@ -57,10 +115,19 @@ const Login = () => {
           >
             로그인
           </button>
-          {/* <button id="loginBtn" onclick="location='./main.html'" disabled>
+          <button
+            onClick={signUp}
+            className={loginBtn ? 'activeLoginBtn' : 'loginBtn'}
+            disabled={
+              inputValues.id === '' || inputValues.pw === '' ? true : false
+            }
+          >
+            회원가입
+          </button>
+        </div>
+        {/* <button id="loginBtn" onclick="location='./main.html'" disabled>
             로그인
           </button> */}
-        </form>
       </div>
       <span className="findPassword">비밀번호를 잊으셨나요?</span>
     </div>
